30894d6df2a5349665c79258c5c1768554106f50
Matyas Krejci <matyas.kre@gmail.com>
2024-09-23 14:55:30 +0200

    update

--- spiral_prototyp.py
+++ spiral_prototyp.py
@@ -3,40 +3,44 @@ def main():
         [2,17,24,23,22,11],
         [3,18,19,20,21,10],
         [4,5,6,7,8,9]]
-    spiral = spiral_matrix(mat)
+    matrice = load('movie-1-spiral.txt')
+    spiral = spiral_matrix(matrice)
     spiral = rev(spiral)
-    res = fill_matrix(spiral, len(mat[0]), len(mat))
-    print(res)
+    res = fill_matrix(spiral, len(matrice[0]), len(matrice))
 
         
 def fill_matrix(matr: list, depth: int, length: int) -> list[list]:
-    result = [[0] * depth for _ in range(length)]
-    top, bottom = 0, length
+        
+    result = [[0] * depth for _ in range(length)] # pripravi matici na
+    top, bottom = 0, length -1
     left, right = 0, depth - 1
     counter = 0
-    while top < bottom and left <= right:
-        for i in range(top, bottom):
+    while top <= bottom and left <= right:
+
+        for i in range(top, bottom+1):
             #print(mat[i][left])
             result[i][left] = matr[counter]
             counter += 1
         left += 1
 
-        for j in range(top+1, right+1):
+        for j in range(left, right+1):
             #print(mat[bottom][j])
-            result[bottom-1][j] = matr[counter]
+            result[bottom][j] = matr[counter]
             counter += 1
         bottom -= 1
+        if left <= right:  # Přidání podmínky pro případ, kdy matici oběhneme
+            for k in range(bottom, top-1, -1):
+                result[k][right] = matr[counter]
+                counter += 1
+            right -= 1
 
-        for k in range(bottom-1, top-1,-1):
-            #print(mat[k][right])
-            result[k][right] = matr[counter]
-            counter += 1
-        right -= 1
-        for l in range(right, top,-1):
-            #print(mat[top][l])
-            result[top][l] = matr[counter]
-            counter += 1
-        top += 1
+        if top <= bottom:  # Přidání podmínky
+            for l in range(right, left-1, -1):
+                result[top][l] = matr[counter]
+                counter += 1
+            top += 1
+
+    upload_to_txt(result, len(result)-1, len(result[0])-1)
     return result
 
 
@@ -46,24 +50,27 @@ def spiral_matrix(matrix: list[list]) -> list:
     left, right = 0, len(matrix[0]) - 1
 
     while top <= bottom and left <= right:
-        for i in range(top, bottom +1):
-            #print(mat[i][left])
+
+        for i in range(top, bottom+1):
             result.append(matrix[i][left])
         left += 1
 
-        for j in range(top+1, right+1):
-            #print(mat[bottom][j])
+        for j in range(left, right+1):
             result.append(matrix[bottom][j])
         bottom -= 1
 
-        for k in range(bottom, top-1,-1):
-            #print(mat[k][right])
-            result.append(matrix[k][right])
-        right -= 1
-        for l in range(right, top,-1):
-            #print(mat[top][l])
-            result.append(matrix[top][l])
-        top += 1
+        if left <= right:  # Přidání podmínky pro případ, kdy matici oběhneme
+            for k in range(bottom, top-1, -1):
+                result.append(matrix[k][right])
+                
+            right -= 1
+
+        if top <= bottom:  # Přidání podmínky
+            for l in range(right, left-1, -1):
+                result.append(matrix[top][l])
+            top += 1
+
+    upload_to_txt(result, len(result), len(result[0]))
     return result
 
 def rev(li: list) -> list:
@@ -71,8 +78,24 @@ def rev(li: list) -> list:
     result = [0] * (leng + 1)
     for i in range(len(li)):
         result[leng-i] = li[i]
-    print("hello")
     return result
+
+def load(filename:str):
+    array: list[list] = []
+    with open(filename) as f:
+       for line in f:
+           line = line.strip() # odebere \n na konci
+           row = list(line) #doplni radky
+           array.append(row)
+    return array
+
+
+def upload_to_txt(image: list, rows, cols):
+    with open('image.txt','w') as f:
+        for i in range(rows):
+            for j in range(cols):
+                f.write(str(image[i][j]))
+            f.write('\n')
 
 if __name__ == "__main__":
     main()
